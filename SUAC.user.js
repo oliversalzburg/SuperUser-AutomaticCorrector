12fdc25e6bb6e4dc0c9bcfa41db252d33f5b257f
Tom Wijsman <TomWij@live.com>
2012-06-11 02:44:19 +0200

    Shortened diff functions.

--- SUAC.user.js
+++ SUAC.user.js
@@ -300,29 +300,16 @@ EmbedFunctionOnPage('diffString', function(o, n) {
 	var out = diff(o == "" ? [] : o.split(/\s+/), n == "" ? [] : n.split(/\s+/));
 	var str = "";
 
-	var oSpace = o.match(/\s+/g);
-	if (oSpace == null) {
-		oSpace = ["\n"];
-	} else {
-		oSpace.push("\n");
-	}
-	var nSpace = n.match(/\s+/g);
-	if (nSpace == null) {
-		nSpace = ["\n"];
-	} else {
-		nSpace.push("\n");
-	}
+	var oSpace = o.match(/\s+/g) || []; oSpace.push("\n");
+	var nSpace = n.match(/\s+/g) || []; nSpace.push("\n");
 
 	if (out.n.length == 0) {
-		for (var i = 0; i < out.o.length; i++) {
+		for (var i = 0; i < out.o.length; i++)
 			str += '<span class="diff-delete">' + out.o[i] + oSpace[i].replace(/\n/g, ' ') + "</span>";
-		}
 	} else {
-		if (out.n[0].text == null) {
-			for (n = 0; n < out.o.length && out.o[n].text == null; n++) {
+		if (out.n[0].text == null)
+			for (n = 0; n < out.o.length && out.o[n].text == null; n++)
 				str += '<span class="diff-delete">' + out.o[n] + oSpace[n].replace(/\n/g, ' ') + "</span>";
-			}
-		}
 
 		for (var i = 0; i < out.n.length; i++) {
 			if (out.n[i].text == null) {
@@ -330,9 +317,9 @@ EmbedFunctionOnPage('diffString', function(o, n) {
 			} else {
 				var pre = "";
 
-				for (n = out.n[i].row + 1; n < out.o.length && out.o[n].text == null; n++) {
+				for (n = out.n[i].row + 1; n < out.o.length && out.o[n].text == null; n++)
 					pre += '<span class="diff-delete">' + out.o[n] + oSpace[n].replace(/\n/g, ' ') + "</span>";
-				}
+
 				str += " " + out.n[i].text + nSpace[i] + pre;
 			}
 		}
@@ -346,63 +333,34 @@ EmbedFunctionOnPage('diff', function(o, n) {
 	var os = new Object();
 
 	for (var i = 0; i < n.length; i++) {
-	if (ns[n[i]] == null)
-		ns[n[i]] = {
-			rows: new Array(),
-			o: null
-		};
+		ns[n[i]] = ns[n[i]] || { rows: new Array(), o: null };
 		ns[n[i]].rows.push(i);
 	}
 
 	for (var i = 0; i < o.length; i++) {
-	if (os[o[i]] == null)
-		os[o[i]] = { rows: new Array(), n: null };
+		os[o[i]] = os[o[i]] || { rows: new Array(), n: null };
 		os[o[i]].rows.push(i);
 	}
 
-	for (var i in ns) {
+	for (var i in ns)
 		if (ns[i].rows.length == 1 && typeof(os[i]) != "undefined" && os[i].rows.length == 1) {
-			n[ns[i].rows[0]] = {
-				text: n[ns[i].rows[0]],
-				row: os[i].rows[0]
-			};
-			o[os[i].rows[0]] = {
-				text: o[os[i].rows[0]],
-				row: ns[i].rows[0]
-			};
+			n[ns[i].rows[0]] = { text: n[ns[i].rows[0]], row: os[i].rows[0] };
+			o[os[i].rows[0]] = { text: o[os[i].rows[0]], row: ns[i].rows[0] };
 		}
-	}
 
-	for (var i = 0; i < n.length - 1; i++) {
+	for (var i = 0; i < n.length - 1; i++)
 		if (n[i].text != null && n[i + 1].text == null && n[i].row + 1 < o.length && o[n[i].row + 1].text == null && n[i + 1] == o[n[i].row + 1]) {
-			n[i + 1] = {
-				text: n[i + 1],
-				row: n[i].row + 1
-			};
-			o[n[i].row + 1] = {
-				text: o[n[i].row + 1],
-				row: i + 1
-			};
+			n[i + 1] = { text: n[i + 1], row: n[i].row + 1 };
+			o[n[i].row + 1] = { text: o[n[i].row + 1], row: i + 1 };
 		}
-	}
 
-	for (var i = n.length - 1; i > 0; i--) {
+	for (var i = n.length - 1; i > 0; i--)
 		if (n[i].text != null && n[i - 1].text == null && n[i].row > 0 && o[n[i].row - 1].text == null && n[i - 1] == o[n[i].row - 1]) {
-			n[i - 1] = {
-				text: n[i - 1],
-				row: n[i].row - 1
-			};
-			o[n[i].row - 1] = {
-				text: o[n[i].row - 1],
-				row: i - 1
-			};
+			n[i - 1] = { text: n[i - 1], row: n[i].row - 1 };
+			o[n[i].row - 1] = { text: o[n[i].row - 1], row: i - 1 };
 		}
-	}
 
-	return {
-		o: o,
-		n: n
-	};
+	return { o: o, n: n };
 });
 
 EmbedFunctionOnPageAndExecute(function() {
